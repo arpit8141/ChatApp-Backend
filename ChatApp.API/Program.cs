7a4249ef023e50a6043102cd2228317139cbd581
Arpit Vaghela <arpit74057@gmail.com>
2025-02-05 19:53:26 +0530

    Added cors policy to allow local frontend requests

--- ChatApp.API/Program.cs
+++ ChatApp.API/Program.cs
@@ -3,22 +3,26 @@ using ChatApp.Infrastructure.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Configure CORS to allow your React app
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy => policy.AllowAnyOrigin()
+    options.AddPolicy("AllowReactApp",
+        policy => policy
+            .WithOrigins("http://localhost:3000") // React app URL
             .AllowAnyMethod()
-            .AllowAnyHeader());
+            .AllowAnyHeader()
+            .AllowCredentials()); // If using credentials
 });
 
+// Add SignalR
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<ChatService>();
 
 var app = builder.Build();
 
-app.UseCors("AllowAll");
+app.UseCors("AllowReactApp"); // Use the updated CORS policy
 
+// Map SignalR Hub
 app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
